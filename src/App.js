9cbd9fe44705f397c12b5dd3a9f7c6bf0a9c4053
Nguyen Van Hien <nguyenvanhien200895@gmail.com>
2018-09-26 17:23:04 +0700

    login basic

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Loading } from './components/common/Loading';
+import Auth from './screens/Auth';
+import LoggedIn from './screens/LoggedIn';
+import deviceStorage from './services/deviceStorage.js';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jwt: '',
+      loading: true
+    }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    this.newJWT = this.newJWT.bind(this);
+    this.deleteJWT = deviceStorage.deleteJWT.bind(this);
+    this.loadJWT = deviceStorage.loadJWT.bind(this);
+    this.loadJWT();
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  newJWT(jwt){
+    this.setState({
+      jwt: jwt
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    if (this.state.loading) {
+      return (
+        <Loading size={'large'} />
+       );
+    } else if (!this.state.jwt) {
+      return (
+        <Auth newJWT={this.newJWT} />
+      );
+    } else if (this.state.jwt) {
+      return (
+        <LoggedIn jwt={this.state.jwt} deleteJWT={this.deleteJWT} />
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
