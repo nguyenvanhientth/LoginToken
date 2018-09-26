32b5b9b69869d2030fb794d70504409aa1c97bce
Nguyen Van Hien <nguyenvanhien200895@gmail.com>
2018-09-26 15:36:26 +0700

    Component First

---
+++ src/components/common/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+const Loading = () => {
+  return (
+    <View>
+      <ActivityIndicator/>
+    </View>
+  );
+};
